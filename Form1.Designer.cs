6a8c47e5de80ccb0453fe091a6f904ef22e43a0d
kyoo1332 <ngoctuanvinh1332@gmail.com>
2023-09-25 22:15:13 +0700

    csharp-ex

---
+++ Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace _22CT115_BuiMinhCuong_122001458_C__OOP_E
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(371, 70);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(371, 115);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 1;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(320, 154);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(75, 23);
+            this.txt1.TabIndex = 2;
+            this.txt1.Text = "Question A:";
+            this.txt1.UseVisualStyleBackColor = true;
+            this.txt1.Click += new System.EventHandler(this.txt1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(320, 183);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "Question B:";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(320, 212);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 4;
+            this.button7.Text = "Question C:";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(320, 241);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 5;
+            this.button8.Text = "Question D:";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(117, 283);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(46, 13);
+            this.label13.TabIndex = 6;
+            this.label13.Text = ".............";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(117, 385);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(37, 13);
+            this.label14.TabIndex = 7;
+            this.label14.Text = "..........";
+            this.label14.Click += new System.EventHandler(this.label14_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(462, 283);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(43, 13);
+            this.label15.TabIndex = 8;
+            this.label15.Text = "............";
+            this.label15.Click += new System.EventHandler(this.label15_Click);
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(462, 385);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(52, 13);
+            this.label16.TabIndex = 9;
+            this.label16.Text = "...............";
+            this.label16.Click += new System.EventHandler(this.label16_Click);
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(211, 70);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(100, 13);
+            this.label17.TabIndex = 10;
+            this.label17.Text = "Enter Row Number:";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(215, 122);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(96, 13);
+            this.label18.TabIndex = 11;
+            this.label18.Text = "Entetr Col Number:";
+            this.label18.Click += new System.EventHandler(this.label18_Click);
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(65, 283);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(46, 13);
+            this.label19.TabIndex = 12;
+            this.label19.Text = "Reply a:";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Location = new System.Drawing.Point(410, 283);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(46, 13);
+            this.label20.TabIndex = 13;
+            this.label20.Text = "Reply c:";
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(410, 385);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(46, 13);
+            this.label21.TabIndex = 14;
+            this.label21.Text = "Reply d:";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(65, 385);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(46, 13);
+            this.label22.TabIndex = 15;
+            this.label22.Text = "Reply b:";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(780, 534);
+            this.Controls.Add(this.label22);
+            this.Controls.Add(this.label21);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button txt1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22CT115_BuiMinhCuong_122001458_C__OOP_E
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int NumRows;
+        int NumCols;
+        int[,] Mang;
+        string Minimi;
+
+
+        private void label9_Click(object sender, EventArgs e)
+        {}
+        private void button4_Click(object sender, EventArgs e)
+        {}
+        private void button3_Click(object sender, EventArgs e)
+        {}
+        private void txt1_Click(object sender, EventArgs e)
+        {
+            NumRows = int.Parse(textBox3.Text);
+            NumCols = int.Parse(textBox4.Text);
+            Mang = new int[NumRows, NumCols];
+            Random rnd = new Random();
+            for (int i = 0; i < NumRows; i++)
+            {
+                for (int j = 0; j < NumCols; j++)
+                {
+                    Mang[i, j] = rnd.Next(0, 100);
+                }
+            }
+            string Results = "  ";
+            for (int i = 0; i < NumRows; i++)
+            {
+                for (int j = 0; j < NumCols; j++)
+                {
+                    Results += Mang[i, j] + "      ";
+                }
+                Results += "\n";
+            }
+            //label9.Text = Results;
+            NumRows = Mang.GetLength(0);
+            NumCols = Mang.GetLength(1);
+            int Max = Mang[0, 0];
+            int MaxRow = 0;
+            int MaxCol = 0;
+
+            for (int i = 0; i < NumRows; i++)
+            {
+                for (int j = 0; j < NumCols; j++)
+                {
+                    if (Mang[i, j] > Max)
+                    {
+                        Max = Mang[i, j];
+                        MaxRow = i;
+                        MaxCol = j;
+                    }
+                }
+            }
+            int MaxRow_2 = MaxRow + 1;
+            int MaxCol_2 = MaxCol + 1;
+            string Results_2 = "Maximum Number Is " + Max + " In Main Diagonal Is: " + "[{" + MaxRow_2 + "}," + "{" + MaxCol_2 + "}]";
+            label13.Text = Results + "\n" + Results_2;
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int Numrow = Mang.GetLength(0);
+            int Numcol = Mang.GetLength(1);
+            int Min = Mang[0, 0];
+            for (int i = 0; i < Numrow && i < Numcol; i++)
+            {
+                if (Mang[i, i] < Min)
+                {
+                    Min = Mang[i, i];
+                }
+
+                Minimi = "Miximum In Main Diagonal Is: " + Min;
+                label14.Text = Minimi;
+            }
+        }
+        private void label10_Click(object sender, EventArgs e)
+        {}
+        private void label9_Click_1(object sender, EventArgs e)
+        {}
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {}
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {}
+        private void button1_Click(object sender, EventArgs e)
+        {}
+        private void button2_Click(object sender, EventArgs e)
+        {}
+        private void label14_Click(object sender, EventArgs e)
+        {}
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int Nrows = Mang.GetLength(0);
+            int Ncols = Mang.GetLength(1);
+            string output = ""; 
+            for (int i = 0; i < Nrows; i++)
+            {
+                int[] Row = new int[Ncols];
+                for (int j = 0; j < Ncols; j++)
+                {
+                    Row[j] = Mang[i, j];
+                }
+                if (i % 2 == 0)
+                {
+                    Array.Sort(Row);
+                }
+                else
+                {
+                    Array.Sort(Row);
+                    Array.Reverse(Row);
+                }
+
+                for (int j = 0; j < Ncols; j++)
+                {
+                    Mang[i, j] = Row[j];
+                    output += Row[j] + "    "; 
+                }
+                output += "\n";
+            }
+            label15.Text = "Array After Sort:\n" + output;
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int numRows = Mang.GetLength(0);
+            int numCols = Mang.GetLength(1);
+            string output = "";
+
+            for (int j = 0; j < numCols; j++)
+            {
+                int[] column = new int[numRows];
+                for (int i = 0; i < numRows; i++)
+                {
+                    column[i] = Mang[i, j];
+                }
+
+                if (j % 2 == 0)
+                {
+                    Array.Sort(column);
+                }
+                else
+                {
+                    Array.Sort(column);
+                    Array.Reverse(column);
+                }
+
+                for (int i = 0; i < numRows; i++)
+                {
+                    Mang[i, j] = column[i];
+                }
+            }
+
+            for (int i = 0; i < numRows; i++)
+            {
+                for (int j = 0; j < numCols; j++)
+                {
+                    output += Mang[i, j]; 
+                    if (j < numCols - 1)
+                    {
+                        output += "     ";
+                    }
+                }
+                output += "\n"; 
+            }
+            label16.Text = "Array After Sort:\n" + output;
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {}
+        private void label11_Click(object sender, EventArgs e)
+        {}
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {}
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {}
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
